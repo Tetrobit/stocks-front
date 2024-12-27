a8b2b067cf2f0bb0b1cccbbaf0d6bfc0e6bba82c
russian_proger <sagitov.2806@mail.ru>
2024-12-28 02:16:51 +0300

    feat: add constants for countries and currencies

---
+++ src/constants/countries.tsx
@@ -0,0 +1,35 @@
+import belarus from '../assets/icons/countries/belarus.png';
+import canada from '../assets/icons/countries/canada.png';
+import china from '../assets/icons/countries/china.png';
+import france from '../assets/icons/countries/france.png';
+import germany from '../assets/icons/countries/germany.png';
+import israel from '../assets/icons/countries/israel.png';
+import italy from '../assets/icons/countries/italy.png';
+import japan from '../assets/icons/countries/japan.png';
+import kazakhstan from '../assets/icons/countries/kazakhstan.png';
+import netherlands from '../assets/icons/countries/netherlands.png';
+import poland from '../assets/icons/countries/poland.png';
+import russia from '../assets/icons/countries/russia.png';
+import spain from '../assets/icons/countries/spain.png';
+import uk from '../assets/icons/countries/uk.png';
+import ukraine from '../assets/icons/countries/ukraine.png';
+import usa from '../assets/icons/countries/usa.png';
+
+export const countries_icons = {
+    'belarus': belarus,
+    'canada': canada,
+    'china': china,
+    'france': france,
+    'germany': germany,
+    'israel': israel,
+    'italy': italy,
+    'japan': japan,
+    'kazakhstan': kazakhstan,
+    'netherlands': netherlands,
+    'poland': poland,
+    'russia': russia,
+    'spain': spain,
+    'uk': uk,
+    'ukraine': ukraine,
+    'usa': usa,
+};
---
+++ src/constants/currencies.tsx
@@ -0,0 +1,13 @@
+export const currencies = {
+    'BYN': ['Белорусский рубль', 'belarus'],
+    'CAD': ['Канадский доллар', 'canada'],
+    'CNY': ['Юань', 'china'],
+    'ILS': ['Шекель', 'israel'],
+    'JPY': ['Иена', 'japan'],
+    'KZT': ['Тенге', 'kazakhstan'],
+    'PLN': ['Злотый', 'poland'],
+    'RUB': ['Рубль', 'russia'],
+    'GBP': ['Фунт', 'uk'],
+    'UAH': ['Гривна', 'ukraine'],
+    'USD': ['Доллар', 'usa'],
+}
