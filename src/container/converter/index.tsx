c76ceaac0db6782bbb19e863996e6c82fd7515b3
russian_proger <sagitov.2806@mail.ru>
2024-12-28 04:17:43 +0300

    feat(converter): add currency converting functionality

--- src/container/converter/index.tsx
+++ src/container/converter/index.tsx
@@ -15,28 +15,56 @@ import { COUNTRIES_ICONS } from '../../constants/countries';
 import { CURRENCIES } from '../../constants/currencies';
 
 const ConverterPage = (): React.ReactElement => {
+  const exrates = React.useMemo(() => {
+    return getExRate();
+  }, []);
 
   const [currencies, setCurrencies] = React.useState(['RUB', 'USD']);
-
-  const [prices, setPrices] = React.useState([1, 1]);
+  const [prices, setPrices] = React.useState([1, exrates[currencies[1]] / exrates[currencies[0]]]);
 
   const handleChangeCurrency = (id: number) => (event: SelectChangeEvent) => {
     let newCurrencies = currencies.slice();
     newCurrencies[id] = event.target.value;
     setCurrencies(newCurrencies);
+
+    let relation = exrates[newCurrencies[1 - id]] / exrates[newCurrencies[id]];
+    let newPrices = prices.slice()
+    newPrices[1 - id] = newPrices[id] * relation;
+    setPrices(newPrices);
   };
+
+  const handleChangePrice = (id: number) => (event: React.ChangeEvent<HTMLInputElement>) => {
+    let newPrices = prices.slice()
+    let relation = exrates[currencies[1 - id]] / exrates[currencies[id]];
+    newPrices[id] = parseFloat(event.target.value);
+    newPrices[1 - id] = newPrices[id] * relation;
+    setPrices(newPrices);
+  }
+
+  const handleRotate = () => {
+    setCurrencies(currencies.reverse().slice());
+    setPrices(prices.reverse().slice());
+  }
 
   return (
     <div className='converter'>
-      { currencies.map((cur, curIndex) => (
-        <React.Fragment key={cur}>
+      { currencies.map((cur, curIndex) => {
+        const cost = (exrates[currencies[1 - curIndex]] / exrates[cur]).toFixed(4);
+        const label = `1 ${cur} = ${cost} ${currencies[1 - curIndex]}`;
+        return <React.Fragment key={cur}>
           <Paper elevation={1}>
             <div className='converter-field'>
               <div className='converter-field-title'>
                 <h3>{['Купить', 'Продать'][curIndex]}</h3>
               </div>
               <div className='converter-field-price'>
-                <TextField label='1 USD = 120 RUB' variant='outlined' />
+                <TextField
+                  value={prices[curIndex]}
+                  onChange={handleChangePrice(curIndex)}
+                  label={label}
+                  variant='outlined'
+                  type='number'
+                />
               </div>
               <div className='converter-field-currency'>
                 <Select
@@ -63,13 +91,13 @@ const ConverterPage = (): React.ReactElement => {
 
           { curIndex == 0 &&
             <div className="converter-rotator">
-              <IconButton>
+              <IconButton onClick={handleRotate}>
                 <SyncAltIcon />
               </IconButton>
             </div>
           }
         </React.Fragment>
-      ))}
+      })}
     </div>
   )
 };
