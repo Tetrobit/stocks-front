c33aeebb49db896e5cbdef2dc3b58a93eb360d06
russian_proger <sagitov.2806@mail.ru>
2024-12-28 03:01:26 +0300

    feat(ex-rate): add api plug

---
+++ src/api.tsx
@@ -0,0 +1,17 @@
+export function getExRate() {
+    const result = {
+        'RUB': 1.0000,
+        'USD': 0.0095,
+        'BYN': 0.0310,
+        'CAD': 0.0140,
+        'CNY': 0.0690,
+        'ILS': 0.0350,
+        'JPY': 1.4900,
+        'KZT': 4.9500,
+        'PLN': 0.0390,
+        'GBP': 0.0075,
+        'UAH': 0.4000,
+    };
+
+    return result
+}
