63a2bdac1fd55cd84978bf4f29584380c083bdc7
p0rtalfan19 <dmitgomonov@gmail.com>
2025-01-27 22:31:58 +0300

    feat: make a loader for buttons

--- src/container/transfer/index.tsx
+++ src/container/transfer/index.tsx
@@ -22,13 +22,15 @@ import React, { useState } from 'react';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import { Snackbar } from '@mui/material';
+import CircularProgress from '@mui/material/CircularProgress';
 
-const TransferPage: React.FC = () => {
+const TransferPage = (): React.ReactElement => {
   const navigate = useNavigate();
 
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [uid, setUid] = useState<string>('');
   const [amount, setAmount] = useState<string>('');
+  const [showFullScreenLoader, setShowFullScreenLoader] = useState<boolean>(false);
 
 
   const handleUidChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -43,10 +45,12 @@ const TransferPage: React.FC = () => {
   const handleTransfer = () => {
     // Логика перевода средств
     setIsLoading(true);
+    setShowFullScreenLoader(true);
     // ...
     setTimeout(() => {
       setIsLoading(false);
-    }, 10000);
+      setShowFullScreenLoader(false);
+    }, 3000);
   };
 
   const handleBack = () => {
@@ -56,6 +60,11 @@ const TransferPage: React.FC = () => {
 
   return (
     <div className="transfer-wrapper">
+      {showFullScreenLoader && (
+        <div className="full-screen-loader">
+          <CircularProgress />
+        </div>
+      )}
       <div className="transfer">
         <h1>Перевод средств</h1>
         <div className="input-group">
@@ -67,11 +76,11 @@ const TransferPage: React.FC = () => {
           <TextField id="outlined-basic" label="RUB" variant="outlined" onChange={handleAmountChange}/>
         </div>
           <Stack spacing={2} direction="row" className="buttons">
-            <Button onClick={handleTransfer} variant="contained">Перевести</Button>
+            <Button onClick={handleTransfer} variant="contained" disabled={isLoading}>Перевести</Button>
             <Button onClick={handleBack}  variant="contained">Назад</Button>
           </Stack>
       </div>
-      <Snackbar open={isLoading}>
+      <Snackbar open={isLoading} autoHideDuration={5000}>
         <Alert icon={<CheckIcon fontSize="inherit" />} severity="success">
           Перевод успешно выполнен.
         </Alert>
