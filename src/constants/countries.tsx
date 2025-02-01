cc0e8e1d32b7ec9e3a2770b2ef9a773d9c4313b2
AliaSab <alia.200220@gmail.com>
2025-02-01 10:54:04 +0300

    feat: import new valute icons

--- src/constants/countries.tsx
+++ src/constants/countries.tsx
@@ -1,35 +1,91 @@
-import belarus from '../assets/icons/countries/belarus.png';
-import canada from '../assets/icons/countries/canada.png';
-import china from '../assets/icons/countries/china.png';
-import france from '../assets/icons/countries/france.png';
-import germany from '../assets/icons/countries/germany.png';
-import israel from '../assets/icons/countries/israel.png';
-import italy from '../assets/icons/countries/italy.png';
-import japan from '../assets/icons/countries/japan.png';
-import kazakhstan from '../assets/icons/countries/kazakhstan.png';
-import netherlands from '../assets/icons/countries/netherlands.png';
-import poland from '../assets/icons/countries/poland.png';
-import russia from '../assets/icons/countries/russia.png';
-import spain from '../assets/icons/countries/spain.png';
-import uk from '../assets/icons/countries/uk.png';
-import ukraine from '../assets/icons/countries/ukraine.png';
-import usa from '../assets/icons/countries/usa.png';
+import AED from '../assets/icons/valutes/AED.png';
+import AMD from '../assets/icons/valutes/AMD.png';
+import AUD from '../assets/icons/valutes/AUD.png';
+import AZN from '../assets/icons/valutes/AZN.png';
+import BGN from '../assets/icons/valutes/BGN.png';
+import BRL from '../assets/icons/valutes/BRL.png';
+import BYN from '../assets/icons/valutes/BYN.png';
+import CAD from '../assets/icons/valutes/CAD.png';
+import CHF from '../assets/icons/valutes/CHF.png';
+import CNY from '../assets/icons/valutes/CNY.png';
+import CZK from '../assets/icons/valutes/CZK.png';
+import DKK from '../assets/icons/valutes/DKK.png';
+import EGP from '../assets/icons/valutes/EGP.png';
+import EUR from '../assets/icons/valutes/EUR.png';
+import GBP from '../assets/icons/valutes/GBP.png';
+import GEL from '../assets/icons/valutes/GEL.png';
+import HKD from '../assets/icons/valutes/HKD.png';
+import HUF from '../assets/icons/valutes/HUF.png';
+import IDR from '../assets/icons/valutes/IDR.png';
+import INR from '../assets/icons/valutes/INR.png';
+import JPY from '../assets/icons/valutes/JPY.png';
+import KGS from '../assets/icons/valutes/KGS.png';
+import KRW from '../assets/icons/valutes/KRW.png';
+import KZT from '../assets/icons/valutes/KZT.png';
+import MDL from '../assets/icons/valutes/MDL.png';
+import NOK from '../assets/icons/valutes/NOK.png';
+import NZD from '../assets/icons/valutes/NZD.png';
+import PLN from '../assets/icons/valutes/PLN.png';
+import QAR from '../assets/icons/valutes/QAR.png';
+import RON from '../assets/icons/valutes/RON.png';
+import RSD from '../assets/icons/valutes/RSD.png';
+import RUB from '../assets/icons/valutes/RUB.png';
+import SEK from '../assets/icons/valutes/SEK.png';
+import SGD from '../assets/icons/valutes/SGD.png';
+import THB from '../assets/icons/valutes/THB.png';
+import TJS from '../assets/icons/valutes/TJS.png';
+import TMT from '../assets/icons/valutes/TMT.png';
+import TRY from '../assets/icons/valutes/TRY.png';
+import UAH from '../assets/icons/valutes/UAH.png';
+import USD from '../assets/icons/valutes/USD.png';
+import UZS from '../assets/icons/valutes/UZS.png';
+import VND from '../assets/icons/valutes/VND.png';
+import ZAR from '../assets/icons/valutes/ZAR.png';
+import UNKNOWN from '../assets/icons/valutes/UNKNOWN.png';
 
 export const COUNTRIES_ICONS = {
-    'belarus': belarus,
-    'canada': canada,
-    'china': china,
-    'france': france,
-    'germany': germany,
-    'israel': israel,
-    'italy': italy,
-    'japan': japan,
-    'kazakhstan': kazakhstan,
-    'netherlands': netherlands,
-    'poland': poland,
-    'russia': russia,
-    'spain': spain,
-    'uk': uk,
-    'ukraine': ukraine,
-    'usa': usa,
+    AED,
+    AMD,
+    AUD,
+    AZN,
+    BGN,
+    BRL,
+    BYN,
+    CAD,
+    CHF,
+    CNY,
+    CZK,
+    DKK,
+    EGP,
+    EUR,
+    GBP,
+    GEL,
+    HKD,
+    HUF,
+    IDR,
+    INR,
+    JPY,
+    KGS,
+    KRW,
+    KZT,
+    MDL,
+    NOK,
+    NZD,
+    PLN,
+    QAR,
+    RON,
+    RSD,
+    RUB,
+    SEK,
+    SGD,
+    THB,
+    TJS,
+    TMT,
+    TRY,
+    UAH,
+    USD,
+    UZS,
+    VND,
+    ZAR,
+    UNKNOWN,
 };
