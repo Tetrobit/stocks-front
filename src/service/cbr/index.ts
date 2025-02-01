de2ee3fdda5581c5e2ab1b49484fbfb14481e7ce
p0rtalfan19 <dmitgomonov@gmail.com>
2025-02-01 12:53:38 +0300

    feat(network.cbr): add getDynamic method

--- src/service/cbr/index.ts
+++ src/service/cbr/index.ts
@@ -3,8 +3,17 @@ import { getConfigValue } from '@brojs/cli';
 
 class CbrService {
     async getDaily() {
-        const response = await network.get('/api/cbr/daily', {
-            withCredentials: true,
+        const response = await network.get('/api/cbr/daily');
+        return response.data;
+    }
+
+    async getDynamic(date_req1, date_req2, val_id) {
+        const response = await network.get('/api/cbr/dynamic', {
+            params: {
+                date_req1,
+                date_req2,
+                val_id,
+            },
         });
         return response.data;
     }
